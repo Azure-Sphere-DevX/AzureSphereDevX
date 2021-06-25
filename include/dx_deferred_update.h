2a0f8506e19025e2e94b6cf523a85cd2703e9f5e
Dave Glover <dglover.au@microsoft.com>
2021-06-25 23:49:40 +1000

    fix to type

--- include/dx_deferred_update.h
+++ include/dx_deferred_update.h
@@ -8,6 +8,6 @@
 #include <errno.h>
 #include <stdbool.h>
 
-void dx_deferredUpdateRegistration(uint32_t (*deferredUpdateCalculateCallback)(unsigned int max_deferral_time_in_minutes),
+void dx_deferredUpdateRegistration(uint32_t (*deferredUpdateCalculateCallback)(uint32_t max_deferral_time_in_minutes),
                                 void (*deferredUpdateNotificationCallback)(SysEvent_UpdateType type, const char *typeDescription,
                                                                            SysEvent_Status status, const char *statusDescription));
