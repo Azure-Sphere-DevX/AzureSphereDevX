c239932357bcdfe64882f1cc543384182dcc9a0a
Dave Glover <dglover@microsoft.com>
2021-10-08 15:45:13 +1100

    Updated PWM Binding

--- include/dx_pwm.h
+++ include/dx_pwm.h
@@ -7,15 +7,22 @@
 #include "stdbool.h"
 #include <applibs/pwm.h>
 
-typedef struct DX_PWM_BINDING
+typedef struct DX_PWM_CONTROLLER
 {
     int fd;
     PWM_ControllerId controllerId;
-    PWM_ChannelId channelId;
-    PwmPolarity pwmPolarity;
     bool initialized;
     char *name;
+} DX_PWM_CONTROLLER;
+
+typedef struct DX_PWM_BINDING
+{
+    DX_PWM_CONTROLLER *pwmController;
+    PWM_ChannelId channelId;
+    PwmPolarity pwmPolarity;
+    char *name;
 } DX_PWM_BINDING;
+
 
 /// <summary>
 /// Set the PWM hertz and duty cycle percentage.
@@ -24,21 +31,21 @@ typedef struct DX_PWM_BINDING
 /// <param name="hertz">Typical hertz range is between 1000 and 5000</param>
 /// <param name="dutyCyclePercentage">Percentage between 0 and 100</param>
 /// <returns></returns>
-bool dx_pwmSetDutyCycle(DX_PWM_BINDING *pwm_binding, uint32_t hertz, uint32_t dutyCyclePercentage);
+bool dx_pwmSetDutyCycle(DX_PWM_BINDING *pwmBinding, uint32_t hertz, uint32_t dutyCyclePercentage);
 
 /// <summary>
 /// Stop and disable the PWM running
 /// </summary>
 /// <param name="pwm_binding"></param>
 /// <returns></returns>
-bool dx_pwmStop(DX_PWM_BINDING *pwm_binding);
+bool dx_pwmStop(DX_PWM_BINDING *pwmBinding);
 
 /// <summary>
 /// Open and initialize the PWM
 /// </summary>
 /// <param name="pwm_binding"></param>
 /// <returns></returns>
-bool dx_pwmOpen(DX_PWM_BINDING *pwm_binding);
+bool dx_pwmOpen(DX_PWM_BINDING *pwmBinding);
 
 /// <summary>
 /// Open a set of PWM Bindings
--- src/dx_pwm.c
+++ src/dx_pwm.c
@@ -1,44 +1,55 @@
 #include "dx_pwm.h"
 
-bool dx_pwmSetDutyCycle(DX_PWM_BINDING *pwm_binding, uint32_t hertz, uint32_t dutyCyclePercentage)
+bool dx_pwmSetDutyCycle(DX_PWM_BINDING *pwmBinding, uint32_t hertz, uint32_t dutyCyclePercentage)
 {
-    if (pwm_binding->initialized && IN_RANGE(dutyCyclePercentage, 0, 100) && hertz < 100000) {
-        PwmState state = {.enabled = true, .polarity = pwm_binding->pwmPolarity};
+    if (pwmBinding->pwmController->initialized && IN_RANGE(dutyCyclePercentage, 0, 100) && IN_RANGE(hertz, 0, 100000))
+    {
+        PwmState state = {.enabled = true, .polarity = pwmBinding->pwmPolarity};
 
         state.period_nsec = 1000000000 / hertz;
         state.dutyCycle_nsec = state.period_nsec * dutyCyclePercentage / 100;
 
-        if (PWM_Apply(pwm_binding->fd, pwm_binding->channelId, &state) == -1) {
+        if (PWM_Apply(pwmBinding->pwmController->fd, pwmBinding->channelId, &state) == -1)
+        {
+            Log_Debug("PWM Channel ID (%d), aka (%s) apply failed\n", pwmBinding->channelId, pwmBinding->name);
             return false;
         }
-    } else {
-        return false;
+        return true;
     }
-
-    return true;
+    return false;
 }
 
-bool dx_pwmStop(DX_PWM_BINDING *pwm_binding)
+bool dx_pwmStop(DX_PWM_BINDING *pwmBinding)
 {
-    PwmState state = {.period_nsec = 100000, .dutyCycle_nsec = 100, .polarity = pwm_binding->pwmPolarity, .enabled = false};
+    if (pwmBinding->pwmController->initialized)
+    {
+        // set arbitarty period and duraction. The important thing is enabled = false
+        PwmState state = {.period_nsec = 100000, .dutyCycle_nsec = 100, .polarity = pwmBinding->pwmPolarity, .enabled = false};
 
-    if (PWM_Apply(pwm_binding->fd, pwm_binding->channelId, &state) == -1) {
-        Log_Debug("PWM Channel ID (%d), aka (%s) apply failed\n", pwm_binding->channelId, pwm_binding->name);
-        return false;
+        if (PWM_Apply(pwmBinding->pwmController->fd, pwmBinding->channelId, &state) == -1)
+        {
+            Log_Debug("PWM Channel ID (%d), aka (%s) apply failed\n", pwmBinding->channelId, pwmBinding->name);
+            return false;
+        }
+        return true;
     }
-    return true;
+    return false;
 }
 
-bool dx_pwmOpen(DX_PWM_BINDING *pwm_binding)
+bool dx_pwmOpen(DX_PWM_BINDING *pwmBinding)
 {
-    if (pwm_binding->initialized) {
+    if (pwmBinding->pwmController->initialized)
+    {
         return true;
     }
 
-    if ((pwm_binding->fd = PWM_Open(pwm_binding->controllerId)) == -1) {
+    if ((pwmBinding->pwmController->fd = PWM_Open(pwmBinding->pwmController->controllerId)) == -1)
+    {
         return false;
-    } else {
-        pwm_binding->initialized = true;
+    }
+    else
+    {
+        pwmBinding->pwmController->initialized = true;
     }
 
     return true;
@@ -46,8 +57,10 @@ bool dx_pwmOpen(DX_PWM_BINDING *pwm_binding)
 
 void dx_pwmSetOpen(DX_PWM_BINDING **pwmSet, size_t pwmSetCount)
 {
-    for (int i = 0; i < pwmSetCount; i++) {
-        if (!dx_pwmOpen(pwmSet[i])) {
+    for (int i = 0; i < pwmSetCount; i++)
+    {
+        if (!dx_pwmOpen(pwmSet[i]))
+        {
             break;
         }
     }
